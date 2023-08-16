988e5a62cf0784ce9566b5f377ad9e721597da6e
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-08-16 12:58:20 +0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = drivers => drivers.slice(0, 2);
+
+const returnLastTwoDrivers = drivers => drivers.slice(2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = multiplier => value => value * multiplier;
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (arrayOfDrivers, fn) => fn(arrayOfDrivers);
+
+describe('index.js', function () {
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+});
